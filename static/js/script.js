2a0f8270c3374261ca53db1f5bcd28ee0c8d695a
Eric Koslow <ekoslow@gmail.com>
2010-10-06 20:40:38 -0400

    Added new Favicon and Content to the footer

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,12 @@
-/* Author: 
+/* Author: Eric Koslow
 
 */
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function() {
+   // do stuff when DOM is ready
+   $("footer").hide();
+   $("#main > a").click(function() {
+     $('footer').show('slow');
+     return false;
+     });
+ });
