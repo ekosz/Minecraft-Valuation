b6f7e1b4804053148989ce6891ceeb8fb9f071ed
Eric Koslow <ekoslow@gmail.com>
2010-10-14 15:42:25 -0400

    Redid database setup.  Made the scraper only scrap, and not store more than it had to.  Shifted some of the calculations to output.rb

--- output.rb
+++ output.rb
@@ -15,30 +15,10 @@ def number_with_delimiter(number, delimiter=",")
 end
 
 def start_up
+
+  ###### LOAD CONSTANTS ########
   yml_location = File.dirname(__FILE__) + '/config/constants.yml'
   constants = YAML::load_file(yml_location)
-  
-  db_location = File.dirname(__FILE__) + '/config/database.yml'
-  db_config = YAML::load_file(db_location)['production']
-
-  conn = PGconn.open(db_config['host'], 5432, nil, nil, db_config['database'], db_config['username'], db_config['password'])
-  sql = "SELECT * FROM data ORDER BY data_id DESC LIMIT 1"
-  values = conn.exec(sql)
-
-  eu_value = values[0]['eu_value']
-  us_value = values[0]['us_value']
-  avg = values[0]['average']
-  @players = values[0]['players'].to_i
-  @top_line = values[0]['top_line'].to_i
-  @last = values[0]['created_at']
-
-  conn.finish
-
-  @since = constants['since']
-
-  @eu_value = '€' + number_with_delimiter(eu_value)
-  @us_value = '$' + number_with_delimiter(us_value)
-  @avg_value = '€' + number_with_delimiter(avg)
 
   @rent = constants['rent']
   @office_space = constants['office_space']
@@ -51,8 +31,39 @@ def start_up
   @legal = constants['legal']
   @legal_cost = (@salary_cost * (5.0/3.0)).to_i + @legal
   @supplies = constants['supplies']
+  @multi = constants['multi']
+  @since = constants['since']
 
   @sga = @rent_cost + @salary_cost + @networking + @legal_cost + @supplies + @misc_sga
+  
+
+  ###### LOAD VERIBLES FROM DATABASE #######
+  db_location = File.dirname(__FILE__) + '/config/database.yml'
+  db_config = YAML::load_file(db_location)['production']
+
+  conn = PGconn.open(db_config['host'], 5432, nil, nil, db_config['database'], db_config['username'], db_config['password'])
+
+  sql = "SELECT players, exchange_rate, created_at FROM data ORDER BY data_id DESC LIMIT 1"
+
+  sql_avg = "SELECT avg(players) FROM data"
+
+  values = conn.exec(sql)
+  avg_res = conn.exec(sql_avg)
+
+  conn.finish
+
+  ###### CALCULATE VALUATION ######
+  @players = values[0]['players'].to_i
+  @top_line = (@players * 9.4 * 365).to_i 
+  avg = ((avg_res[0]['avg'].to_f * 9.4 * 365).to_i - @sga) * @multi
+  eu_value = (@top_line - @sga) * @multi
+  us_value = (eu_value * values[0]['exchange_rate'].to_f).to_i
+  @last = values[0]['created_at']
+
+  ###### MAKE SOME NUMBERS READABLE ########
+  @eu_value = '€' + number_with_delimiter(eu_value)
+  @us_value = '$' + number_with_delimiter(us_value)
+  @avg_value = '€' + number_with_delimiter(avg)
 
 end
 
--- scraper.rb
+++ scraper.rb
@@ -25,7 +25,8 @@ class Scraper
   end
 
   def start
-
+    
+    ###### PARSE MINECRAFT SITE ######
     doc = open("http://www.minecraft.net/stats.jsp") { |f| Hpricot(f) }
 
     @text = (doc/"/html/body/div[2]/p").inner_html
@@ -36,52 +37,17 @@ class Scraper
       puts "Could not find string"
       return
     end
-
-    yaml_location = File.dirname(__FILE__) + '/config/constants.yml'
-    if ( data = YAML::load_file(yaml_location) )
-      @since = data['since']
-      rent = data['rent']                 #Euros per sqft per year
-      office_space = data['office_space'] #Square Feet for 6 people
-      salary = data['salary']             #Pretty good salary
-      employees = data['employees']       #As Notch has said
-      networking = data['networking']     #Monthly cost * Number of months
-      legal = data['legal']               #Paying lawyers
-      supplies = data['supplies']         #Office supplies
-      misc_sga = data['misc_sga']          #For things I can't remember
-    else
-      puts "Could not load YAML file"
-      return
-    end
-
-
-    office_cost = rent * office_space
-    salary_cost = salary * employees
-    payroll_tax = (salary_cost * (5.0/3.0)).to_i
-
-    sga = misc_sga + office_cost + salary_cost + networking + legal + payroll_tax + supplies
-
-    @top_line = (@players * 9.4 * 365).to_i
-
-    @eu_value = (@top_line - sga) * 3 #Times 3 multiplyer
-    @us_value = (@eu_value * curr_exchange_rate).to_i 
-
-    @avg = @eu_value #In case one can't retrerive it from the database
-
-    get_last_avg = "SELECT average FROM data ORDER BY data_id DESC LIMIT 1"
-
-    sql_insert = "INSERT INTO data (top_line, eu_value, us_value, average, players, created_at) VALUES ($1, $2, $3, $4, $5, now())"
+    
+    ###### INSERT VARIBLES INTO DATABASE ######
+    sql = "INSERT INTO data (players, exchange_rate, created_at) VALUES ($1, $2, now())"
 
     db_location = File.dirname(__FILE__) + '/config/database.yml'
     db_config = YAML::load_file(db_location)['production']
     conn = PGconn.open(db_config['host'], 5432, nil, nil, db_config['database'], db_config['username'], db_config['password'])
 
-    res = conn.exec(get_last_avg)
-    @avg = res[0]['average'].to_i rescue @eu_value #In case one can't retrerive it from the database
-
-    conn.exec(sql_insert,[@top_line, @eu_value, @us_value, ((@eu_value+@avg)/2).to_i, @players])
+    conn.exec(sql, [@players, curr_exchange_rate])
 
     conn.finish
 
-    @eu_value
   end
 end
